46f7292170175e49bdee6946ef7ad1b13e23915e
Tulio Nogueira <tuliomn@Thot.local>
2020-11-19 23:02:04 -0300

    Part 2 - Django tutorial

--- django_lti_tests/settings.py
+++ django_lti_tests/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from polls.apps import PollsConfig
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
